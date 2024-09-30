f2db1781e25dc291ae0d1ee757e8554d4eb72572
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-09-30 20:02:14 +0700

    Refactor start_app.sh script: Update symlink creation for better clarity and maintainability

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,18 +3,13 @@
 # Navigate to the app directory
 cd /home/ubuntu
 
-# Backup the current live version by renaming the symlink
-if [ -L "app_live" ]; then
-  unlink app_live
-  mv app_live app_old_$(date +%Y%m%d%H%M%S)
-fi
-
 # Create a new symlink for app_live pointing to the new deployment
-ln -sfn /home/ubuntu/app_new /home/ubuntu/app_live
+mv ./app_new ./app_live
 
-cd /home/ubuntu/app_live
+# Navigate to the app directory
+cd ./app_live
 
-# Reload the app using PM2
+# Start the PM2 process
 pm2 start ecosystem.config.js
 
 # Save the PM2 process list to ensure it restarts on reboot
--- scripts/stop_and_clean.sh
+++ scripts/stop_and_clean.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Ensure the ubuntu user owns the app_new directory before removing it
-sudo chown -R ubuntu:ubuntu /home/ubuntu/app_new
-
-# Remove the app_new directory
-rm -rf /home/ubuntu/app_new
-
-# Recreate the app_new directory
-mkdir /home/ubuntu/app_new
-
-# Ensure the new directory is writable by the ubuntu user
-chmod -R 775 /home/ubuntu/app_new
-
 # Stop the PM2 process to free up the directory
 pm2 stop landing-page || true
 pm2 delete landing-page || true
+
+# Navigate to the app directory
+sudo chown -R ubuntu:ubuntu /home/ubuntu/app_live
+
+# Stop the PM2 process to free up the directory
+rm -rf ./app_live
