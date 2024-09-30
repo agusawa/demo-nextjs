cf9ddf00d8788b53d71ea3d24170b1554ac747a1
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-09-30 18:37:06 +0700

    Refactor clean.sh script to ensure new deployment directory is writable

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Create a new deployment directory
+# Ensure the ubuntu user owns the app_new directory before removing it
+sudo chown -R ubuntu:ubuntu /home/ubuntu/app_new
+
+# Remove the app_new directory
 rm -rf /home/ubuntu/app_new
+
+# Recreate the app_new directory
 mkdir /home/ubuntu/app_new
 
-# Ensure the new directory is writable
-sudo chmod -R 775 /home/ubuntu/app_new
+# Ensure the new directory is writable by the ubuntu user
+chmod -R 775 /home/ubuntu/app_new
