3be8215843762af45b773a3f040de83af0c89e6f
Agus Stiawan <agus.stiawan2211@gmail.com>
2024-09-30 20:26:13 +0700

    Refactor link text in chart page component: Update link text to "BACK TOO HOMEEEEE" for better clarity and user experience.

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -38,7 +38,7 @@ export default function Charts() {
   return (
     <>
       <div className="max-w-6xl mx-auto">
-        <Link href="/">HOMEEEEE</Link>
+        <Link href="/">BACK TOO HOMEEEEE</Link>
       </div>
       <div className="chart-wrapper mx-auto flex max-w-6xl flex-col flex-wrap items-start justify-center gap-6 p-6 sm:flex-row sm:p-8">
         <div className="grid w-full gap-6 sm:grid-cols-2 lg:max-w-[22rem] lg:grid-cols-1 xl:max-w-[25rem]">
